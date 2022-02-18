5c8d03831cb75f391a3e3c125f6a88ecba2401d4
Amadeu Martim <amadeu@infowsoftware.com.br>
2022-02-18 12:48:41 -0300

    Main

---
+++ ConsoleApp1/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT
+{
+    class Menu
+    {
+        public static void Main()
+        {
+            Tree tree = new Tree();
+            tree.AddNodeTree(10);
+            tree.AddNodeTree(20);
+            tree.AddNodeTree(30);
+        }
+    }
+}
+
--- ConsoleApp1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using BT;
-
-Tree newTree = new Tree();
-newTree.AddNodeTree(10);
-newTree.AddNodeTree(20);
-newTree.AddNodeTree(30);
