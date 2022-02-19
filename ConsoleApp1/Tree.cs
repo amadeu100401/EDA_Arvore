029dc445ac6e05d2a06f4b8dc6354651a9561a84
Amadeu Martim <amadeu@infowsoftware.com.br>
2022-02-19 11:58:07 -0300

    Métodopara retornar o nodo raiz

--- ConsoleApp1/Tree.cs
+++ ConsoleApp1/Tree.cs
@@ -55,5 +55,11 @@ namespace BT
                 }
             return true;
         }
+
+        public Node ReturnRoot()
+        {
+            Console.WriteLine(NodeRoot.Value);
+            return this.NodeRoot;
+        }
     }
 }
