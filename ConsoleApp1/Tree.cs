816273d06214946e2547da5d77e390fc9254e511
Amadeu Martim <amadeu@infowsoftware.com.br>
2022-02-22 21:16:31 -0300

    Ajustes

--- ConsoleApp1/Tree.cs
+++ ConsoleApp1/Tree.cs
@@ -14,6 +14,7 @@ namespace BT
             public int Value { get; set; }
             public Node? LeftNode { get; set; }
             public Node? RightNode { get; set; }
+            public int level { get; set; }
         }
 
         static Node newNode(int value)
