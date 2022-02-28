73034c9f6b99417801d1d2da79dbd0beb10e873c
Amadeu Martim <amadeu@infowsoftware.com.br>
2022-02-28 17:16:27 -0300

    Refatoração de código

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            Node root = null;
+            root = tree.InsertNode(root, 10);
+            tree.InsertNode(root, 20);
+            tree.InsertNode(root, 30);
+            tree.InsertNode(root, 40);
+
+            Console.WriteLine(root.LeftNode.Value);
+        }
+    }
+
+}
--- ConsoleApp1/Tree.cs
+++ ConsoleApp1/Tree.cs
@@ -3,175 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static BT.Tree;
 
 namespace BT
 {
-    public class Tree
+    class Tree
     {
-        public class Node
+        public Node newNode(int value)
         {
-            public int Value { get; set; }
-            public Node? LeftNode { get; set; }
-            public Node? RightNode { get; set; }
-            public int level { get; set; }
+            Node node = new Node();
+            node.Value = value;
+            node.LeftNode = null;
+            node.RightNode = null;
+            return node;
         }
 
-        static Node newNode(int value)
-        {
-            Node tn = new Node();
-            tn.Value = value;
-            tn.LeftNode = null;
-            tn.RightNode = null;
-
-            return tn;
-        }
-
-        public static Node InsertNode (Node root,int value)
+        public Node InsertNode(Node root, int value)
         {
             if (root == null)
             {
                 root = newNode(value);
-                Console.WriteLine("Raiz criada");
             }
-            else if (value < root.Value)
+            else
             {
                 if (root.LeftNode == null)
                 {
                     root.LeftNode = newNode(value);
-                    Console.WriteLine("Esquerdo criado");
+                    root.Grade += 1;
                 }
-
-                else
-                {
-                    InsertNode(root.LeftNode,value);
-                }
-            }
-            else if (value > root.Value)
-            {
-                if (root.RightNode == null) 
+                else if (root.RightNode == null)
                 {
                     root.RightNode = newNode(value);
+                    root.Grade += 1;
                 }
-
                 else
                 {
-                    InsertNode(root.RightNode, value);
+                    InsertNode(root.LeftNode, value);
                 }
-            }   
-
+            }
             return root;
         }
-
-        private static int Verify (Node root, int value)
-        {
-            var GradeN = 0;
-
-            if (root.Value == value)
-            {
-                if (root.LeftNode == null && root.RightNode == null)
-                {
-                    return GradeN;
-                }
-                else if (root.LeftNode == null && root.RightNode != null || root.LeftNode != null && root.RightNode == null )
-                {
-                    GradeN = 1;
-                    return GradeN;
-                }
-                else
-                {
-                    GradeN = 2;
-                    return GradeN;
-                }
-            }
-            else
-            {
-                if (root.LeftNode != null)
-                {
-                    if (value > root.LeftNode.Value)
-                    {
-                        Verify(root.RightNode, value);
-                    }
-                    else
-                    {
-                        Verify(root.LeftNode, value);
-                    }
-
-                }
-                else if (root.RightNode != null)
-                {
-                    if (value < root.RightNode.Value)
-                    {
-                      Verify(root.LeftNode, value);
-                    }
-                    else
-                    {
-                        Verify(root.RightNode, value);
-                    }
-                }
-            }
-            return GradeN;
-        }
-
-        public string LeafNode (Node root, int value)
-        {
-            var gradeNode = Verify(root, value);
-            if (gradeNode == 0)
-            {
-                return "O nó é folha";
-            }
-            return "O nó não é folha";
-        }
-        
-        public int GradeNode(Node root, int value )
-        {
-            if (root is null)
-            {
-                Console.WriteLine("A árvore está vazia");
-            }
-            else
-            {
-                var obj = Verify(root, value);
-                Console.WriteLine(obj); 
-                return obj;
-            }
-            return 98687;
-        }
-
-        private void ShowTree(Node root)
-        {
-            if (root == null)
-            {
-                return;
-            }
-            else
-            {
-                ShowTree(root.LeftNode);
-                Console.WriteLine(root.Value + " ");
-                ShowTree(root.RightNode);
-            }
-
-        }
-
-        public void Display(Node root)
-        {
-            ShowTree(root);
-        }
-
-        public static void Main(String[] args)
-        {
-            Tree bst = new Tree();
-            Node root = null;
-            root = InsertNode(root,10);
-            InsertNode(root,20);
-            InsertNode(root, 5);
-            InsertNode(root, 40);
-            InsertNode(root, 30);
-
-            bst.GradeNode(root, 10);
-            Console.WriteLine(bst.LeafNode(root,10));
-            bst.Display(root);
-        }
-
     }
 }
